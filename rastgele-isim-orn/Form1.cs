e82547373b3b5fc5e1334020a1b190062ea9c881
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-11-20 10:41:44 +0300

    Add files via upload

---
+++ rastgele-isim-orn/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastgele_isim_orn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan=txtAra.Text;
+            bool kontrol=false;
+
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (aranan == listBox1.Items[i].ToString())
+                {
+                    kontrol=true;
+                    break;
+                }
+            }
+            if (kontrol == true)
+            {
+                MessageBox.Show("öğrenci bulundu");
+            }
+            else
+            {
+                MessageBox.Show("öğrenci bulunamadı");
+            }
+        }
+    }
+}
